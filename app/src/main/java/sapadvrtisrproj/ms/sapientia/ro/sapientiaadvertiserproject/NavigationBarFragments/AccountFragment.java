56b043ea2ccd3df5b566703385ab43fd0b4ea3cd
gergelysz <gszblcs97@gmail.com>
2018-11-15 15:30:26 +0200

    small update on accountfragment

--- app/src/main/java/sapadvrtisrproj/ms/sapientia/ro/sapientiaadvertiserproject/NavigationBarFragments/AccountFragment.java
+++ app/src/main/java/sapadvrtisrproj/ms/sapientia/ro/sapientiaadvertiserproject/NavigationBarFragments/AccountFragment.java
@@ -16,7 +16,11 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        
+        View view =  inflater.inflate(R.layout.fragment_account, container, false);
+
+        // TODO ...
+
+        return view;
     }
 }
