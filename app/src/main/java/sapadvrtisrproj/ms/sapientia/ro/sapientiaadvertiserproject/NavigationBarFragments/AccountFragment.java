ba206d838b0464ffef726d3f96c5ecc7e2ecce0e
kelemantamas <kelemantamas@users.noreply.github.com>
2019-01-13 19:08:03 +0200

    get from database by id

--- app/src/main/java/sapadvrtisrproj/ms/sapientia/ro/sapientiaadvertiserproject/NavigationBarFragments/AccountFragment.java
+++ app/src/main/java/sapadvrtisrproj/ms/sapientia/ro/sapientiaadvertiserproject/NavigationBarFragments/AccountFragment.java
@@ -1,11 +1,12 @@
 package sapadvrtisrproj.ms.sapientia.ro.sapientiaadvertiserproject.NavigationBarFragments;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,21 +16,26 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.IOException;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import sapadvrtisrproj.ms.sapientia.ro.sapientiaadvertiserproject.AdsActivity;
 import sapadvrtisrproj.ms.sapientia.ro.sapientiaadvertiserproject.Data.User;
 import sapadvrtisrproj.ms.sapientia.ro.sapientiaadvertiserproject.MainActivity;
 import sapadvrtisrproj.ms.sapientia.ro.sapientiaadvertiserproject.R;
-import sapadvrtisrproj.ms.sapientia.ro.sapientiaadvertiserproject.RegistrationActivity;
-import sapadvrtisrproj.ms.sapientia.ro.sapientiaadvertiserproject.RegistrationActivity;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -47,36 +53,60 @@ public class AccountFragment extends Fragment {
     private FirebaseFirestore db;
 
     public static final int GET_FROM_GALLERY = 1;
+    public static final String TAG = "AccountFragment";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         
         final View view =  inflater.inflate(R.layout.fragment_account, container, false);
 
+        AdsActivity ref = (AdsActivity) AccountFragment.this.getActivity();
+        final String userId = ref.getUserId();
+        Log.d(TAG,"userid " + userId);
+
+        //adb lekerdezes
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+//        Query query = databaseReference.child("users").equalTo(userId);
+//        query.ge
+
+        db.collection("users").document(userId).get().addOnSuccessListener(documentSnapshot -> {
+            user.setFirstName(documentSnapshot.getString("firstName"));
+            user.setLastName(documentSnapshot.getString("lastName"));
+            user.setAddress(documentSnapshot.getString("address"));
+            user.setEmail(documentSnapshot.getString("email"));
+            user.setImageURL(documentSnapshot.getString("imageURL"));
+            user.setPhoneNumber(documentSnapshot.getString("phoneNumber"));
+
+            Log.d(TAG, "uclass" + user.getFirstName() + " " + user.getLastName());
+        });
+        //Log.d(TAG, "teszt " + query);
+        Log.d(TAG, "uclass" + user.getFirstName() + " " + user.getLastName());
+
         editFname = view.findViewById(R.id.fragment_acc_edit_fname);
         if(user.getFirstName() != null)
         {
-            editFname.setHint(user.getFirstName());
+            editFname.setText(user.getFirstName());
         }
         editLname = view.findViewById(R.id.fragment_acc_edit_lname);
         if(user.getLastName() != null)
         {
-            editLname.setHint(user.getLastName());
+            editLname.setText(user.getLastName());
         }
         editEmail = view.findViewById(R.id.fragment_acc_edit_email);
         if(user.getEmail() != null)
         {
-            editEmail.setHint(user.getEmail());
+            editEmail.setText(user.getEmail());
         }
         editPhone = view.findViewById(R.id.fragment_acc_edit_phone);
         if(user.getPhoneNumber() != null)
         {
-            editPhone.setHint(user.getPhoneNumber());
+            editPhone.setText(user.getPhoneNumber());
         }
         editAddress = view.findViewById(R.id.fragment_acc_edit_address);
         if(user.getAddress() != null)
         {
-            editAddress.setHint(user.getAddress());
+            editAddress.setText(user.getAddress());
         }
         saveBtn = view.findViewById(R.id.fragment_acc_save);
 
@@ -90,19 +120,22 @@ public class AccountFragment extends Fragment {
         });
 
         changePicture = view.findViewById(R.id.fragment_acc_picture);
-        changePicture.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-               //startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GET_FROM_GALLERY);
-
-            }
+        changePicture.setOnClickListener(v -> {
+           //startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), GET_FROM_GALLERY);
 
         });
 
+//        changePicture = view.findViewById(R.id.fragment_acc_picture);
+//        changePicture.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//               startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+//            }
+//        });
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,38 +146,18 @@ public class AccountFragment extends Fragment {
                 user.setAddress(editAddress.getText().toString());
                 user.setAddress(editEmail.getText().toString());
 
-                db.collection("users").document(/*userid*/).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                db.collection("users").document(userId).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
 
                         Toast.makeText(getActivity(), "Adatok sikeresen frissítve", Toast.LENGTH_SHORT).show();
                     }
                 });
-            }
-        });
 
-        changePicture = view.findViewById(R.id.fragment_acc_picture);
-        changePicture.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-               startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
-            }
-        });
-
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                user.setFirstName(editFname.getText().toString());
-                user.setLastName(editLname.getText().toString());
-                user.setPhoneNumber(editPhone.getText().toString());
-                user.setAddress(editAddress.getText().toString());
-                user.setAddress(editEmail.getText().toString());
-
-                db.collection("users").document(/*userid*/).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                db.collection("users").document(userId).set(user).addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onSuccess(Void aVoid) {
-
-                        Toast.makeText(getActivity(), "Adatok sikeresen frissítve", Toast.LENGTH_SHORT).show();
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), "Az adatokat nem sikerült feltölteni", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
